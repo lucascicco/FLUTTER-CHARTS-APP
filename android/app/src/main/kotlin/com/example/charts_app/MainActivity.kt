fc871918e062b7e0fd7117ce7ab3a8e16103dec3
lucacicco <lucasciccomy@gmail.com>
2020-12-21 12:59:37 -0300

    first_commit

---
+++ android/app/src/main/kotlin/com/example/charts_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.charts_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/auth.dart';
+
+import './screens/splash_screen.dart';
+import './screens/auth_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+      ],
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'My charts',
+          theme: ThemeData(
+            primarySwatch: Colors.deepOrange,
+            accentColor: Colors.deepOrangeAccent,
+            fontFamily: 'Lato',
+          ),
+          home: auth.isAuth
+              ? Text('Authenticated')
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen(),
+                ),
+          routes: {},
+        ),
+      ),
+    );
+  }
+}
